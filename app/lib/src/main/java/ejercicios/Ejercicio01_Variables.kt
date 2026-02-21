d45abe4fbbab7a95b26df8fe2a1e3d0708e444d2
Alan Ayala <aar80810@lasallebajio.edu.mx>
2026-02-20 21:30:42 -0600

    Ejercicio 1

--- app/lib/src/main/java/ejercicios/Ejercicio01_Variables.kt
+++ app/lib/src/main/java/ejercicios/Ejercicio01_Variables.kt
@@ -16,6 +16,6 @@ fun main() {
     val estatura: Double = 1.78
     val esEstudiante: Boolean = true
 
-    // TODO: Usa println con string templates para imprimir el mensaj
+    // TODO: Usa println con string templates para imprimir el mensajee
     println("Me llamo $nombre, tengo $edad anos, mido $estatura y es $esEstudiante que soy estudiante")
 }
